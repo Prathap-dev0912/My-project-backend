edf0468ec8c63c335a69c4826731be7a11de8337
Prathap Ryzer <prathap@ryzer.app>
2026-03-30 12:17:51 +0530

    add node engine for vercel

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,7 @@ let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: '*',
   });
@@ -20,5 +21,6 @@ export default async function handler(req: any, res: any) {
   if (!server) {
     server = await bootstrap();
   }
-  return server(req, res);
+
+  return server(req, res); // ✅ keep this
 }
